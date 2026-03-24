20c918d86091b6d297b30a9cd4890b1d83a11f9d
shrodingerhuman <singhtamana277@gmail.com>
2026-03-25 03:59:28 +0530

    Fixed the empty Image filed when no image is provided with a default image

--- types/category.ts
+++ types/category.ts
@@ -1,10 +1,14 @@
 import { z } from "zod";
-const categorySchema = z.object({
+export const categorySchema = z.object({
     name: z.string().min(2, "Name must be at least 2 characters"),
     description: z.string().min(10, "Description is a bit too short"),
     icon: z.string().min(1, "Please select an icon name (e.g., Briefcase)"),
     color: z.string().default("#1D3557"),
-    image: z.string().min(5, "Please use a valid image url").default("adnan-saifee-zmr9TeA7WjU-unsplash_jpxf7l"),
+    image: z.string().optional().transform((val) =>
+        (!val || val === "")
+            ? "adnan-saifee-zmr9TeA7WjU-unsplash_jpxf7l"
+            : val
+    ),
 });
 
-type CategoryFormValues = z.infer<typeof categorySchema>;
+export type CategoryFormValues = z.infer<typeof categorySchema>;
