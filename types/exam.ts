d45b3fef5dfc062d121fe29ee477dd60b76823d6
shrodingerhuman <singhtamana277@gmail.com>
2026-04-01 02:04:48 +0530

    Added Category Number to Exam Schema and zod

--- types/exam.ts
+++ types/exam.ts
@@ -9,6 +9,7 @@ export const examSchema = z.object({
     name: z.string().min(2, { message: 'Exam name is required' }),
     examCategoryId: z.string().min(1, 'Please select a parent category'),
     description: z.string().min(10, { message: 'Description must be at least 10 characters' }),
+    categoryNumber: z.string().optional().nullable(),
     tags: z.array(z.string().min(1, 'Tag cannot be empty')).min(1, { message: 'At least one tag is required' }),
     duration: z.coerce.number().min(1, { message: 'Duration must be a positive number' }),
     totalMarks: z.coerce.number().min(1, { message: 'Total marks must be a positive number' }),
